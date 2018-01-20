8e8add819bf68cd9e3365af3599543727afd80b1
David Kuruvilla <dapawn@gmail.com>
2018-01-20 12:03:27 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,9 @@
-class Model
+class Dog
+  attr_accessor :name, :breed, :age
+  @@all = []
 
+  def initialize (name: nil, breed: nil, age: nil)
+    @name, @breed, @age = name, breed, age
+    @@all << self
+  end
 end
